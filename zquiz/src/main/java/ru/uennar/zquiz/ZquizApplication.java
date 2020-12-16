a93acc56a309e802bc98a12f01b443a2decdc9ce
Khalim Galimov <khalimgal@gmail.com>
2020-12-16 21:05:15 +0300

    Zquiz project creating

---
+++ zquiz/src/main/java/ru/uennar/zquiz/ZquizApplication.java
@@ -0,0 +1,13 @@
+package ru.uennar.zquiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZquizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZquizApplication.class, args);
+	}
+
+}
---
+++ zquiz/src/test/java/ru/uennar/zquiz/ZquizApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.uennar.zquiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZquizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
